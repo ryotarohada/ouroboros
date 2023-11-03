facdfd2800c16870f10b2dfc732e6aca397f1ca9
Ryotaro Hada <67140275+ryotarohada@users.noreply.github.com>
2023-11-03 18:00:07 +0900

    update

--- src/components/elements/HomeHeader/index.tsx
+++ src/components/elements/HomeHeader/index.tsx
@@ -1,4 +1,4 @@
-import { HamburgerIcon } from '@chakra-ui/icons'
+import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons'
 import {
 	HStack,
 	List,
@@ -7,18 +7,32 @@ import {
 	useColorMode,
 	useDisclosure,
 	chakra,
+	Text,
+	CircularProgress,
 } from '@chakra-ui/react'
 import { BasicModal } from '../BasicModal'
 import { Href } from '../Href'
 import { APP_URLS } from '@/constants/appUrls'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useSearchParams } from 'react-router-dom'
 import { useStorage } from '@/hooks/useStorage'
+import { useState } from 'react'
+import ReactAudioPlayer from 'react-audio-player'
+import { usePageVisibility } from 'react-page-visibility'
 
 export const HomeHeader = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure()
 	const { colorMode, toggleColorMode } = useColorMode()
 	const location = useLocation()
+	const [searchParams] = useSearchParams()
 	const { model, onSetModel } = useStorage()
+	const isVisible = usePageVisibility()
+	const [isSpeak, setIsSpeak] = useState(false)
+	const [isAudioLoading, setIsAudioLoading] = useState(true)
+
+	const storyId = searchParams.get('id')
+	const pageId = searchParams.get('page')
+
+	console.log(storyId, pageId)
 
 	return (
 		<>
@@ -47,6 +61,24 @@ export const HomeHeader = () => {
 							size="lg"
 						/>
 					</ListItem>
+					<ListItem display="flex" justifyContent="space-between" alignItems="center" w="full">
+						<chakra.span fontWeight="bold" display="inline-flex" flexDir="column">
+							読み上げ機能（α版）
+							<chakra.span fontSize={12}>※ このページの文章を読み上げます</chakra.span>
+							<chakra.span fontSize={12}>※ 現時点では一部ページのみの実装です</chakra.span>
+						</chakra.span>
+						<Switch
+							isChecked={isSpeak}
+							onChange={() => {
+								if (!isSpeak) {
+									scrollTo(0, 0)
+								}
+								onClose()
+								setIsSpeak(!isSpeak)
+							}}
+							size="lg"
+						/>
+					</ListItem>
 					{location.pathname !== APP_URLS.HOME && (
 						<ListItem>
 							<Href link={APP_URLS.HOME}>作品一覧へ戻る</Href>
@@ -54,6 +86,48 @@ export const HomeHeader = () => {
 					)}
 				</List>
 			</BasicModal>
+
+			{isSpeak && (
+				<HStack
+					bgColor="purple.600"
+					position="fixed"
+					w="calc(100% - 32px)"
+					borderRadius="md"
+					bottom={4}
+					left={4}
+					px={4}
+					py={4}
+					justify="space-between"
+					color="white"
+					fontFamily="Noto Sans JP, sans-serif"
+				>
+					{isAudioLoading ? (
+						<HStack spacing={6}>
+							<CircularProgress isIndeterminate color="gray.600" />
+							<Text>音声ファイル取得中</Text>
+						</HStack>
+					) : (
+						<Text>読み上げ中</Text>
+					)}
+					<CloseIcon
+						onClick={() => {
+							setIsSpeak(false)
+						}}
+					/>
+					<ReactAudioPlayer
+						src={`/story/${storyId}/${pageId}.wav`}
+						autoPlay
+						loop={false}
+						onCanPlay={() => {
+							setIsAudioLoading(false)
+						}}
+						onEnded={() => {
+							setIsSpeak(false)
+						}}
+						muted={!isVisible}
+					/>
+				</HStack>
+			)}
 		</>
 	)
 }
--- src/components/layouts/DefaultLayout/index.tsx
+++ src/components/layouts/DefaultLayout/index.tsx
@@ -8,7 +8,16 @@ type Props = {
 export const DefaultLayout = ({ children, p = 4, ...props }: Props) => {
 	return (
 		<Container p={p} maxW={800} {...props}>
-			<chakra.div position="fixed" top={0} left={0} right={0} margin="auto" width="full" maxW={800}>
+			<chakra.div
+				position="fixed"
+				top={0}
+				left={0}
+				right={0}
+				margin="auto"
+				width="full"
+				maxW={800}
+				zIndex={10}
+			>
 				<HomeHeader />
 			</chakra.div>
 			<chakra.div mt={10}>{children}</chakra.div>
